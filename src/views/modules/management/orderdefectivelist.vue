0e9048aa19010c128c951ee47754e583a977c8ef
15701579485@163.com <15701579485@163.com>
2019-08-07 17:48:00 +0800

    去掉了 测试调试的代码。

--- src/views/modules/management/orderdefectivelist.vue
+++ src/views/modules/management/orderdefectivelist.vue
@@ -722,7 +722,6 @@
       },
       // 获取数据列表
       getDataList () {
-        console.log('发送请求了。')
         this.dataListLoading = true
         this.$http({
           url: this.$http.adornUrl('/management/orderdefectivelist/list'),
@@ -736,9 +735,7 @@
             'limit': this.pageSize
           })
         }).then(({data}) => {
-          console.log(data.page.list)
           if (data && data.code === 0) {
-            console.log('返回')
             this.dataList = data.page.list
             this.totalPage = data.page.totalCount
           } else {
